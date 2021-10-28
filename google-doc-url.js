562bf9aed5e13d62b52e493a0208d774ac7b210e
samueloakford <76913227+samueloakford@users.noreply.github.com>
2021-10-28 11:29:36 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1VfNX2g-RqZPcow5NioIS4a_QENkjsRJgtDmFuOjF8fQ/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = '09fb44046d0885d94429a868d67385378c175aa9';
+var googleApiKey = 'AIzaSyA9xfGKolnib4jZ2dmSi9B7K4RehGNuEC0';
